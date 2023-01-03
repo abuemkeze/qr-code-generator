9e238890eb4aef77fa75530ce2f17987cbdfed38
MUGI <abuemkezemueghe@gmail.com>
2023-01-03 01:53:42 +0100

    generate qr-code function

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('generate-form');
+const qr = document.getElementById('qr-code');
+
+const onGenerateSubmit = (e) => {
+    e.preventDefault();
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if (url === '') {
+        alert("Please Enter a URL")
+    } else {
+        generateQRcode(url,size);
+    }
+
+}
+
+const generateQRcode = (url, size) => {
+
+    const qrcode = new QRCode('qrcode',{
+        text: url,
+        width: size,
+        height: size
+    });
+}
+
+form.addEventListener('submit', onGenerateSubmit);
